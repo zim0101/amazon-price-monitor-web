909a7a5bf016c0ee6f1f1808209d6c20183a35b1
zim0101 <cybertronix.4406@gmail.com>
2020-09-14 21:02:32 +0600

    Add product price DTO

---
+++ src/main/java/backend/product/dto/ProductPriceDTO.java
@@ -0,0 +1,47 @@
+package backend.product.dto;
+
+public class ProductPriceDTO {
+    private int id;
+    private String name;
+    private double price;
+    private String date;
+
+    public ProductPriceDTO(int id, String name, double price, String date) {
+        this.id = id;
+        this.name = name;
+        this.price = price;
+        this.date = date;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+}
