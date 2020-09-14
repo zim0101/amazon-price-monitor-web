0cd302ef69549c304dde99b3e882ebd538cd2b6b
zim0101 <cybertronix.4406@gmail.com>
2020-09-14 21:03:21 +0600

    Add ProductPriceHistoryQuery file

---
+++ src/main/java/backend/product/query/ProductPriceHistoryQuery.java
@@ -0,0 +1,24 @@
+package backend.product.query;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+public class ProductPriceHistoryQuery {
+
+    public static PreparedStatement getHistory(Connection connection, int id) throws SQLException {
+        String query = "select\n" +
+                "       products.id,\n" +
+                "       products.name,\n" +
+                "       product_price_histories.price,\n" +
+                "       product_price_histories.date\n" +
+                "from products\n" +
+                "    left join product_price_histories on products.id = product_price_histories.product_id\n" +
+                "where products.id = ?\n" +
+                "order by date;";
+        PreparedStatement preparedStatement = connection.prepareStatement(query);
+        preparedStatement.setInt(1, id);
+
+        return preparedStatement;
+    }
+}
