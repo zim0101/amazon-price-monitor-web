f48b3a09415685b82942a07067681d63171a7ac4
zim0101 <cybertronix.4406@gmail.com>
2020-09-14 21:06:21 +0600

    Add products table migration

---
+++ src/main/java/backend/database/migrations/ProductTableMigration.java
@@ -0,0 +1,30 @@
+package backend.database.migrations;
+
+import backend.database.migrate.Migration;
+import backend.database.utils.QueryExecutor;
+
+public class ProductTableMigration extends QueryExecutor implements Migration {
+
+    private static final String UP_QUERY = "create table products"
+            + "("
+            + " id int auto_increment primary key,"
+            + " name varchar(100) not null,"
+            + " price double not null,"
+            + " site_name varchar(100) not null,"
+            + " url text not null,"
+            + " price_selector varchar(100) not null,"
+            + " name_selector varchar(100) not null"
+            + ");";
+
+    private  static final String DOWN_QUERY = "DROP TABLE IF EXISTS products;";
+
+    @Override
+    public void up() {
+        executeQuery(UP_QUERY);
+    }
+
+    @Override
+    public void down() {
+        executeQuery(DOWN_QUERY);
+    }
+}
