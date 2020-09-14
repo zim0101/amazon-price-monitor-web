9765b73f2ae1857610e9f86b0d61b91cef52014f
zim0101 <cybertronix.4406@gmail.com>
2020-09-14 21:06:33 +0600

    Add product_price_histories table migration

---
+++ src/main/java/backend/database/migrations/ProductPriceHistoryMigration.java
@@ -0,0 +1,27 @@
+package backend.database.migrations;
+
+import backend.database.migrate.Migration;
+import backend.database.utils.QueryExecutor;
+
+public class ProductPriceHistoryMigration extends QueryExecutor implements Migration {
+
+    private static final String UP_QUERY = "create table product_price_histories "
+            + "("
+            + "id int auto_increment primary key,"
+            + "product_id int not null,"
+            + "price double not null,"
+            + "date datetime not null"
+            + ");";
+
+    private  static final String DOWN_QUERY = "DROP TABLE IF EXISTS product_price_histories;";
+
+    @Override
+    public void up() {
+        executeQuery(UP_QUERY);
+    }
+
+    @Override
+    public void down() {
+        executeQuery(DOWN_QUERY);
+    }
+}
